a67dc9986b86fe599200cb3ecf5f8befc61074de
Mindmie <dreamiing.allday@gmail.com>
2017-09-14 11:31:32 +0700

    Initial commit

---
+++ mobile/src/main/java/com/example/mindmie/healthme/HealthMeMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mindmie.healthme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HealthMeMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_me_main);
+    }
+}
